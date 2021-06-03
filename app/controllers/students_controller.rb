32bbfaccbed9b5d5bbb511e6189d584a398d9641
prahal <vprahaladhan@gmail.com>
2021-06-03 14:14:14 +1000

    commit tree 2c17bf663c5bec1bfdbc469befbdb49aacbcf388

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
+require 'pry'
+
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
   end
 
   def show
+    
+  end
+
+  def activate
+    binding.pry
+    @student.active = !@student.active
+    puts @student
+    redirect_to @student
   end
 
   private
